5021d4ebf03b1d2572555c32e76947fcf34b8ecd
Travis Brown <travis.brown@nidavellir.in>
2022-09-22 14:48:36 -0400

    Adding IServiceCollection extensions

---
+++ ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+// Define our namespace
+namespace SyncStream.Validator.Hostname;
+
+/// <summary>
+/// This class maintains the the IServiceCollection extensions
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// This method adds the HostnameValidatorServiceDatabaseUpdateWorker to the services collection
+    /// </summary>
+    /// <param name="instance">The instance of IServiceCollection</param>
+    public static void UseSyncStreamHostnameValidator(this IServiceCollection instance)
+    {
+        // Register our background worker
+        instance.AddHostedService<HostnameValidatorServiceDatabaseUpdateWorker>();
+    }
+}
