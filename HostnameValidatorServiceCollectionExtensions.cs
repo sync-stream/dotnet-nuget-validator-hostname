f56bfe3f5e2d086b13915c0db42d9253b51c3e01
Travis Brown <travis.brown@nidavellir.in>
2022-09-22 14:53:25 -0400

    Updating IServiceCollection extensions to fluid interface

--- HostnameValidatorServiceCollectionExtensions.cs
+++ HostnameValidatorServiceCollectionExtensions.cs
@@ -6,15 +6,19 @@ namespace SyncStream.Validator.Hostname;
 /// <summary>
 /// This class maintains the the IServiceCollection extensions
 /// </summary>
-public static class ServiceCollectionExtensions
+public static class HostnameValidatorServiceCollectionExtensions
 {
     /// <summary>
     /// This method adds the HostnameValidatorServiceDatabaseUpdateWorker to the services collection
     /// </summary>
     /// <param name="instance">The instance of IServiceCollection</param>
-    public static void UseSyncStreamHostnameValidator(this IServiceCollection instance)
+    /// <returns><paramref name="instance" /> for a fluid interface</returns>
+    public static IServiceCollection UseSyncStreamHostnameValidator(this IServiceCollection instance)
     {
         // Register our background worker
         instance.AddHostedService<HostnameValidatorServiceDatabaseUpdateWorker>();
+
+        // We're done, return the instance
+        return instance;
     }
 }
